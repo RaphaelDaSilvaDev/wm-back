8866e3fa1e5b0c4079bcab8414835b6c4dac411e
RaphaelMW <raphael@mundowap.com.br>
2023-02-16 14:49:42 -0300

    startup project with express, prisma, docker, postgress

---
+++ prisma/migrations/20230216174726_create_user/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "permition" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
